05a00bd688ce298171d2e841444751d239db45d9
william_doyle <mcbidebilly@gmail.com>
2023-09-22 15:43:50 -0400

    added some 'special' stuff

--- test/Lunar.ts
+++ test/Lunar.ts
@@ -2,24 +2,44 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Lunar", async function () {
+    let lunar: any;
+
     it("simple test", async function () {
-        const lunar = await ethers.deployContract("Lunar");
+        lunar = await ethers.deployContract("Lunar");
         await lunar.waitForDeployment();
         const lunarAddress = await lunar.getAddress();
         console.log(`Lunar deployed to: ${lunarAddress}`)
 
-        const currentPhase : string = await lunar.currentPhase();
+        const currentPhase: string = await lunar.currentPhase();
         console.log(`Current phase:     ${currentPhase}`);
 
-        const currentFraction : bigint = await lunar.currentFrac();
+        const currentFraction: bigint = await lunar.currentFrac();
         console.log(`Current fraction:  ${currentFraction}`);
 
-        it("NFT Contract", async function () {
-            const nftContract = await ethers.deployContract("LunarTokens", [lunar.getAddress()]);
 
-
-
-            
-        })
     });
+
+    it("NFT Contract", async function () {
+        const prefix = "https://williamdoyle.ca/lunar-tokens/standard-version/";
+        const postfix = ".json";
+        const nftContract = await ethers.deployContract("LunarTokens", [lunar.getAddress(), [
+            `${prefix}New_Moon${postfix}`,
+            `${prefix}Waxing_Crescent${postfix}`,
+            `${prefix}First_Quarter${postfix}`,
+            `${prefix}Waxing_Gibbous${postfix}`,
+            `${prefix}Full_Moon${postfix}`,
+            `${prefix}Waning_Gibbous${postfix}`,
+            `${prefix}Last_Quarter${postfix}`,
+            `${prefix}Waning_Crescent${postfix}`,
+        ]]);
+
+        const owner = await nftContract.owner();
+        console.log(`NFT Contract owner: ${owner}`);
+
+        await nftContract.safeMint(owner);
+        const tokenId = await nftContract.tokenOfOwnerByIndex(owner, 0);
+
+        const tokenURI = await nftContract.tokenURI(tokenId);
+        console.log(`Token URI: ${tokenURI}`);
+    })
 });
